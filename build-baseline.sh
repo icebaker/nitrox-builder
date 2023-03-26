4d3f6d3ffb254e8190a8c7caa3fc921d460609ed
icebaker <icebaker@proton.me>
2023-03-26 19:36:53 -0300

    ensure .env

--- build-baseline.sh
+++ build-baseline.sh
@@ -1,3 +1,7 @@
+if [ ! -f ".env" ]; then
+  cp .env.example .env
+fi
+
 APP=baseline SERVICE=discovery ./build-ruby.sh
 APP=baseline SERVICE=connector ./build-ruby.sh
 APP=baseline SERVICE=proxy ./build-ruby.sh
--- build-ui.sh
+++ build-ui.sh
@@ -1,1 +1,5 @@
+if [ ! -f ".env" ]; then
+  cp .env.example .env
+fi
+
 APP=ui SERVICE=ui ./build-svelte.sh
